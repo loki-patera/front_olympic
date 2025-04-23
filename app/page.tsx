8b69efb6ea75884fe612e874236d3cb32d676cba
loki-patera <boulet.g@outlook.be>
2025-04-23 17:03:00 +0200

    Create header

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import { Header } from "./components/Header";
+
+/**
+ * Composant `Home` pour afficher la page d'accueil de l'application
+ */
 export default function Home() {
 
   return (
 
     <>
-      
+      <Header />
     </>
   )
 }
